192607a16687fa54d3f8b051a1c7d0e72917797f
Andrea Corna <corninos@corninos-macbook-pro.local>
2017-01-09 23:42:38 +0100

    ToAvoid

--- MoveBehavior.swift
+++ MoveBehavior.swift
@@ -14,8 +14,9 @@ class MoveBehavior: GKBehavior {
     init(agent: [GKAgent], target: [GKAgent], obstacles: [GKPolygonObstacle], path: [GKGraphNode]) {
         super.init()
         let cgpath = GKPath(graphNodes: path, radius: 0)
-        setWeight(100, for: GKGoal(toStayOn: cgpath, maxPredictionTime: 0.9))
-        setWeight(100, for: GKGoal(toFollow: cgpath, maxPredictionTime: 0.9, forward: true))
+        setWeight(100, for: GKGoal(toStayOn: cgpath, maxPredictionTime: 1.5))
+        setWeight(100, for: GKGoal(toFollow: cgpath, maxPredictionTime: 1.5, forward: true))
+        //toAvoid sembra non funzioanre ..
         setWeight(20, for: GKGoal(toAvoid: obstacles, maxPredictionTime: 5))
         setWeight(1, for: GKGoal(toInterceptAgent: target.first!, maxPredictionTime: 1))
 
